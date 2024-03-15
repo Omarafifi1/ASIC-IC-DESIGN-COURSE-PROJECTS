0220d0e49aa6e22541089c2c83cb1b8c9c4315c2
omar mohamed <106562602+Omarafifi1@users.noreply.github.com>
2024-03-15 05:26:56 +0200

    Add files via upload

---
+++ VERILOG/UART_TX/parity_calc.v
@@ -0,0 +1,15 @@
+module parity_calc #(parameter data_width=8)(
+input [data_width-1:0]p_data,
+input data_valid,busy,parity_type,
+input clk,rst_n,
+output reg parity_bit
+);
+
+always @(posedge clk , negedge rst_n) begin
+    case ({data_valid,busy,parity_type})
+        3'b100:parity_bit<=^(p_data);
+        3'b101:parity_bit<=^~(p_data); 
+        default:parity_bit<=parity_bit;
+    endcase        
+end
+endmodule
